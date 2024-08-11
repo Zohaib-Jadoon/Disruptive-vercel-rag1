676b4fa2f78c6091f01e830eac768503b620582d
Abduullaahh <hafizabdullahtahir@gmail.com>
2024-08-12 02:39:00 +0500

    minor fix

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <>
       <section className="login grid grid-cols-1 lg:grid-cols-2">
-        <div className="loginImg flex flex-col md:flex-row   items-start  text-3xl md:text-5xl space-y-3 md:space-y-0 md:space-x-3">
+        <div className="loginImg flex flex-col md:flex-row items-start text-3xl md:text-5xl space-y-3 md:space-y-0 md:space-x-3 hidden md:block" style={{ backgroundImage: 'url("/assets/Robot blur.png")'}}>
           <div className=" flex items-center mt-[3%] ml-[5%] gap-2">
             <img src={icon} className="w-12 h-12 md:w-15 md:h-15 items-start" />
             <span className="text-white text-[18px] text-center md:mt-0">
